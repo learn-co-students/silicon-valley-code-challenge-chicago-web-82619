bb3807aed4559ff1f39aff82c5c6a2a7c4e789c0
Ashwin Kannan <classifieddatabank@Ashwins-MacBook-Pro.local>
2019-08-30 12:15:18 -0500

    lab complete

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,54 @@
 class Startup
+  @@all = []
+  attr_reader :name, :founder, :domain
+
+  def initialize(name, founder, domain)
+    @name = name
+    @founder = founder
+    @domain = domain
+    @@all << self
+  end
+
+  def pivot(domain, name)
+    @name = name
+    @domain = domain
+  end
+
+  def sign_contract(vc, type, amount)
+    FundingRound.new(self, vc, type, amount)
+  end
+
+  # helper
+  def funding_rounds
+    FundingRound.all.select {|round| round.startup == self}
+  end
+  
+  def num_funding_rounds
+    funding_rounds.length
+  end
+
+  def total_funds
+    funding_rounds.map {|round| round.investment}.sum
+  end
+
+  def investors
+    funding_rounds.map {|round| round.venture_capitalist}.uniq
+  end
+
+  def big_investors
+    investors.select {|investor| VentureCapitalist.tres_commas_club.include?(investor)}
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_founder(founder)
+    all.find {|startup| startup.founder == founder}
+  end
+
+  def self.domains
+    all.map {|startup| startup.domain}.uniq
+  end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,9 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
-
+fb = Startup.new('Facebook', 'Zuckerberg', 'social media')
+apple = Startup.new('Apple', 'Jobs', 'computers')
+gates = VentureCapitalist.new('Gates', 100000000000)
+bezos = VentureCapitalist.new('Bezos', 70000000)
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
