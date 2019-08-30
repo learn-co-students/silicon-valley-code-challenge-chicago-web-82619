b62dccc92ccfc865b0d2d7ee9e018994a736ca30
github username <github email address>
2019-08-30 13:00:29 -0500

    Done.

--- tools/console.rb
+++ tools/console.rb
@@ -1,11 +1,15 @@
 require_relative '../config/environment.rb'
+require_relative '../app/models/startup.rb'
+require_relative '../app/models/funding_round.rb'
+require_relative '../app/models/venture_capitalist.rb'
 
 def reload
   load 'config/environment.rb'
 end
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+
+s1 = Startup.new("Silicon","GJ","Tech")
+s2 = Startup.new("Silicon","GJ","Tech")
+s3 = Startup.new("Silicon","GJ","Tech")
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
